c3553f2833f54ceca25ed08d467a19f144f0236a
Roberto Emilio <robertoemilio1@hotmail.com>
2025-07-29 15:09:12 -0300

    Desafio conluido - Roberto - 29-07-2025

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -38,49 +38,68 @@ public class ValidacoesListaTests
     public void NaoDeveConterONumero10NaLista()
     {
         //TODO: Implementar método de teste
+        //Implementado - Roberto - 29/07/2025
 
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
         var numeroParaProcurar = 10;
 
         // Act
+        //Roberto - 29/07/2025
+        var resultado = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);
 
         // Assert
+        //Roberto - 29/07/2025
+        Assert.False(resultado);
     }
 
-    //TODO: Corrigir a anotação [Fact]
+    //TODO: Corrigir a anotação 
+
+    
+    [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
         //TODO: Implementar método de teste
+        //Implementado - Roberto - 29/07/2025
 
         // Arrange
         var lista = new List<int> { 5, 7, 8, 9 };
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
         
         // Act
+        //Roberto - 29/07/2025
+        var resultado = _validacoes.MultiplicarNumerosLista(lista,2);
 
         // Assert
+        //Roberto - 29/07/2025
+        Assert.Equal(resultadoEsperado, resultado);
     }
 
     [Fact]
     public void DeveRetornar9ComoMaiorNumeroDaLista()
     {
         //TODO: Implementar método de teste
+        //Implementado - Roberto - 29/07/2025
 
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
 
         // Act
+        //Roberto - 29/07/2025
+        var resultado = _validacoes.RetornarMaiorNumeroLista(lista);
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+        //Roberto - 29/07/2025
+        Assert.Equal(resultado, 9);
+        
     }
 
     [Fact]
     public void DeveRetornarOitoNegativoComoMenorNumeroDaLista()
     {
         //TODO: Implementar método de teste
+        //Implementado - Roberto - 29/07/2025
 
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
@@ -90,6 +109,7 @@ public class ValidacoesListaTests
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        //Roberto - 29/07/2025
+        Assert.Equal(resultado, -8);
     }
 }
--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -5,42 +5,49 @@ namespace TestesUnitarios.Desafio.Tests;
 public class ValidacoesStringTests
 {
     private ValidacoesString _validacoes = new ValidacoesString();
+    
+        [Fact]
+        public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+        {
+            //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
+            //Implementado - Roberto - 29/07/2025
 
-    [Fact]
-    public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
-    {
-        //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
+            // Arrange
+            //Roberto - 29/07/2025
+            var texto = "sapato";
+            var resultadoEsperado = 6;
 
-        // Arrange
-        var texto = "a";
-        var resultadoEsperado = 0;
+            // Act
+            var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
 
-        // Act
-        var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
+            // Assert
+            Assert.Equal(resultadoEsperado, resultado);
+        }
 
-        // Assert
-        Assert.Equal(resultadoEsperado, resultado);
-    }
+        [Fact]
+        public void DeveContemAPalavraQualquerNoTexto()
+        {
+            //Implementado - Roberto - 29/07/2025
+            // Arrange
+            var texto = "Esse é um texto qualquer";
+            var textoProcurado = "qualquer";
 
-    [Fact]
-    public void DeveContemAPalavraQualquerNoTexto()
-    {
-        // Arrange
-        var texto = "Esse é um texto qualquer";
-        var textoProcurado = "qualquer";
+            //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
+            //Roberto - 29/07/2025
+            // Act
+            var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
-        //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
-        // Act
-         _validacoes.ContemCaractere(texto, textoProcurado);
-
-        // Assert
-        //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
-        Assert.True(true);
-    }
+            // Assert
+            //Roberto - 29/07/2025
+            //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
+            Assert.True(resultado);
+        }
+    
 
     [Fact]
     public void NaoDeveConterAPalavraTesteNoTexto()
     {
+        //Implementado - Roberto - 29/07/2025
         // Arrange
         var texto = "Esse é um texto qualquer";
         var textoProcurado = "teste";
@@ -50,17 +57,21 @@ public class ValidacoesStringTests
 
         // Assert
         //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
-        Assert.False(true);
+        //Roberto - 29/07/2025
+        Assert.False(resultado);
     }
 
-    //TODO: Corrigir a anotação [Fact]
+    //TODO: Corrigir a anotação 
+    
+     [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
         //TODO: Corrigir a variável "textoProcurado" seção Arrange
+        //Implementado - Roberto - 29/07/2025
 
         // Arrange
         var texto = "Começo, meio e fim do texto procurado";
-        var textoProcurado = "teste";
+        var textoProcurado = "procurado";//Roberto - 29/07/2025
 
         // Act
         var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
